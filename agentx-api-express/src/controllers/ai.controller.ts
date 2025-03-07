42731234df1820c549cc6e3ef0fff39085668d4c
layinka <xcelsis02@gmail.com>
2025-03-07 17:49:41 +0100

    Compound plugin

--- agentx-api-express/src/controllers/ai.controller.ts
+++ agentx-api-express/src/controllers/ai.controller.ts
@@ -18,13 +18,13 @@ import * as readline from 'node:readline/promises';
 import { createOpenAICompatible } from '@ai-sdk/openai-compatible';
 import { createWalletClient } from "viem";
 import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
-import { sepolia } from "viem/chains";
+import { sepolia, sonic,hardhat, sonicTestnet } from "viem/chains";
 import { http } from "viem";
 import { getOnChainTools } from "@goat-sdk/adapter-vercel-ai";
 import { viem } from "@goat-sdk/wallet-viem";
 import { EVMWalletClient, sendETH, } from "@goat-sdk/wallet-evm";
 
-import { MODE, USDC, erc20 } from "@goat-sdk/plugin-erc20";
+import { MODE, Token, erc20 } from "@goat-sdk/plugin-erc20";
 import { kim } from '@goat-sdk/plugin-kim';
 import { UserPromptDTO } from '@/dtos/ai.dto';
 import { randomUUID } from 'node:crypto';
@@ -32,6 +32,72 @@ import { PluginBase } from '@goat-sdk/core';
 import { createMistral } from '@ai-sdk/mistral';
 import { defillama, DefiLlamaPlugin } from '@/goat_plugins/defi-llama';
 import { debridge } from "@goat-sdk/plugin-debridge";
+import { compound_v2 } from '@/goat_plugins/compound_v2/src';
+
+
+export const USDC: Token = {
+  decimals: 6,
+  symbol: "USDC",
+  name: "USDC",
+  chains: {
+      "1": {
+          contractAddress: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+      },
+      "10": {
+          contractAddress: "0x0b2c639c533813f4aa9d7837caf62653d097ff85",
+      },
+      "137": {
+          contractAddress: "0x3c499c542cef5e3811e1192ce70d8cc03d5c3359",
+      },
+      "8453": {
+          contractAddress: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+      },
+      "84532": {
+          contractAddress: "0x036CbD53842c5426634e7929541eC2318f3dCF7e",
+      },
+      "11155111": {
+          contractAddress: "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238",
+      },
+      "34443": {
+          contractAddress: "0xd988097fb8612cc24eeC14542bC03424c656005f",
+      },
+      "31337": {
+          contractAddress: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+      },
+  },
+};
+
+export const DAI: Token = {
+  decimals: 18,
+  symbol: "DAI",
+  name: "DAI",
+  chains: {
+      "1": {
+          contractAddress: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+      },
+      "10": {
+          contractAddress: "0x0b2c639c533813f4aa9d7837caf62653d097ff85",
+      },
+      "137": {
+          contractAddress: "0x3c499c542cef5e3811e1192ce70d8cc03d5c3359",
+      },
+      "8453": {
+          contractAddress: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+      },
+      "84532": {
+          contractAddress: "0x036CbD53842c5426634e7929541eC2318f3dCF7e",
+      },
+      "11155111": {
+          contractAddress: "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238",
+      },
+      "34443": {
+          contractAddress: "0xd988097fb8612cc24eeC14542bC03424c656005f",
+      },
+      "31337": {
+          contractAddress: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+      },
+  },
+};
 
 // const privateKey = generatePrivateKey()
 // console.log('priva key:', process.env.WALLET_PRIVATE_KEY??'No Key in ENV' )
@@ -42,22 +108,9 @@ const account = privateKeyToAccount(
 
 const walletClient = createWalletClient({
   account: account,
-  transport: http('https://ethereum-sepolia-rpc.publicnode.com'),
-  chain: sepolia,
+  transport: http('http://localhost:8545'),//'https://ethereum-sepolia-rpc.publicnode.com'
+  chain: hardhat,
 });
-
-
-
-
-const hyperbolicProvider = createOpenAICompatible({
-  name: 'hyperbolic',
-  apiKey: process.env.HYPERBOLIC_API_KEY,
-  baseURL: 'https://api.hyperbolic.xyz/v1',
-  
-});
-
-
-
 
 
 export class AIController {
@@ -90,11 +143,11 @@ export class AIController {
 
       const mistralAi = createMistral();
 
-      const messages1: Message[]= [
+      const messages: Message[]= [
         {
           id: randomUUID(),
           role: 'system',
-          content: 'You are an expert crypto defi agent.',
+          content: 'You are an expert crypto defi agent.You work on the hardhat chain. You know how to help people get their ERC20 Balances, their ETH balance and General ERC20 token information like name, symbol, supply. You also know how to get information about defi yield opportunities from defillama.',
         },
         {
           id: randomUUID(),
@@ -111,9 +164,10 @@ export class AIController {
           //@ ts -ignore
           // sendETH(), 
           sendETH()  as unknown as PluginBase<EVMWalletClient>, 
-          erc20({ tokens: [USDC, MODE] }), 
+          erc20({ tokens: [USDC, DAI, MODE] }), 
           defillama(),
-          debridge()
+          // debridge(),
+          compound_v2()
           // kim() 
         ]
       });
@@ -121,9 +175,9 @@ export class AIController {
       const result = streamText({
         //   model: openai('gpt-4o'),
         model:mistralAi('mistral-large-latest', {
-            safePrompt: true, // optional safety prompt injection
+            // safePrompt: true, // optional safety prompt injection
           }), // hyperbolicProvider('meta-llama/Meta-Llama-3.1-70B-Instruct'),
-        // messages: messages1,
+        messages: messages,
         //  [
         //   {
         //     role: 'system',
@@ -161,13 +215,13 @@ export class AIController {
   
         //   ...onchainTools
         // },
-        maxSteps: 3,
+        maxSteps: 15,
         onStepFinish: step => {
           
           console.log(step.finishReason, step.stepType, JSON.stringify(step.response, null, 2));
         },
   
-        prompt: promptData.prompt?? "What's my wallet Address?",
+        // prompt: promptData.prompt?? "What's my wallet Address?",
       });
   
       let fullResponse = '';
--- agentx-api-express/src/goat_plugins/defi-llama/defi-llama.plugin.ts
+++ agentx-api-express/src/goat_plugins/defi-llama/defi-llama.plugin.ts
@@ -1,22 +1,26 @@
 import {type Chain, PluginBase } from '@goat-sdk/core';
 import { DefiLlamaService } from './def-llama.service';
-import { base, optimism, sonic, sonicTestnet } from 'viem/chains';
+import { base, hardhat, optimism, sonic, sonicTestnet } from 'viem/chains';
 
 
 const supportedChains = [
   base,
   optimism,
   sonic, 
-  sonicTestnet
+  sonicTestnet,
+  hardhat,
+
 ]
 
 export class DefiLlamaPlugin extends PluginBase {
   constructor() {
-    super('defi-llama', [new DefiLlamaService()]);
+    super('defillama', [new DefiLlamaService()]);
+    
   }
 
   supportsChain(chain: Chain): boolean {
-    return chain.type === "evm" && (chain.id in supportedChains);
+    
+    return chain.type === "evm" && supportedChains.map(c=>c.id).some(ss=>ss==chain.id);
   }
   // supportsChain = () => true;
 }
