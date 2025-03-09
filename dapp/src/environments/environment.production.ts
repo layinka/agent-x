7d98756365e870c59b004cfcd3d4044b76964a73
layinka <xcelsis02@gmail.com>
2025-03-10 00:27:29 +0100

    add file

---
+++ dapp/src/environments/environment.production.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: true,
+    BaseAPiUrl : "http://localhost:3000", // "http://localhost:3100/v1", // http://localhost:3100/v1
+    reownProjectId: '6dc075707b4e66bff8df286aab204770',
+    
+    cryptoCompareApiKey: '091',
+    graphQl: 'http://localhost:42069/',
+    googleClientId: '801622939250-jj7325606efc6if31gscvrhbsp8sot2g.apps.googleusercontent.com'
+};
